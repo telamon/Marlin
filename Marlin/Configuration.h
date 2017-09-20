0bfdada95987772eb7e7e0fa7e944f051a61733b
Tony Ivanov <telamohn@gmail.com>
2017-09-20 23:33:58 +0200

    No wonder the Z-maxfeedrate was ignored, it's specified in SECONDS not MINUTES per second. e.g. 2mm/s equals F120 option to G0/1 ops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -498,7 +498,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 350, 10, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 350, 2.8 , 45 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
